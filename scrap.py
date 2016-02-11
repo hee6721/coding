0584fb999dc95aa8df5190e942e0e091069c5a5e
hee6721 <heesics@gmail.com>
2016-02-11 16:04:07 -0600

    Create scrap.py

---
+++ scrap.py
@@ -0,0 +1,29 @@
+##__author__ = 'Victor'                                                                                         
+import urllib                                                                                                   
+import re                                                                                                       
+##url = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid=883177'                                            
+url = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid=553625'                                              
+def crawlURL(addedURL):                                                                                         
+    url = addedURL                                                                                              
+    html = urllib.urlopen(url).read()                                                                           
+                                                                                                                
+    teacherData = re.findall(r'\">(.*?)</',html)                                                                
+                                                                                                                
+    output = ''                                                                                                 
+    addStuff = 0                                                                                                
+    for x in xrange(len(teacherData)):                                                                          
+        if teacherData[x] == 'Submit a Correction':                                                             
+            output = 'professor: '                                                                              
+            for y in xrange(4):                                                                                 
+                output += teacherData[x-8+y] + ' '                                                              
+            addStuff = 1                                                                                        
+        elif teacherData[x] == 'Helpfulness' and addStuff == 1:                                                 
+            output += ': Overall quality: '+ str(teacherData[x-2]) + ': Average grade: ' + str(teacherData[x-1]) + ': Helpfulness: ' + teacherData[x+1]                                                                        
+        elif teacherData[x] == 'Easiness' and addStuff == 1:                                                    
+            output += ': Easiness: ' + str(teacherData[x+2])                                                    
+        elif teacherData[x] == 'Clarity' and addStuff == 1:                                                     
+            output += ': Clarity: ' + str(teacherData[x+1])                                                     
+            addStuff = 1                                                                                        
+            break                                                                                               
+    print output                                                                                                
+crawlURL(url) 
